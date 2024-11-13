ed0b28a649e9bd51de816da99f32b6893154dc5d
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2024-11-13 13:25:57 +0100

    Updates: parallelisation, comments, plotting shifts

--- mito-agents.c
+++ mito-agents.c
@@ -1,10 +1,12 @@
+// code to simulate ATP concentration dynamics in a model 2D cell
+
 // assume cell production is 10^9 ATP/s (https://www.molbiolcell.org/doi/10.1091/mbc.e14-09-1347)
 // assume all OXPHOS: 100 mitos -> production (balanced) = 10^7 ATP/s/mito
 // diffusion rate somewhere around (5?)*5*1.5e2 length^2 / s
 // solution concentration ~ 10^5 ATP/length^2
 // typical cellular ATP conc = 1-10mM (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8615055/)
 // 1mM ATP = 10^-3 mol dm-3 = 10^-3 mol (10^15 um^3)-1 = 1e-3 * 6e23 / 1e15 = 6e5 molecules / um^3
-// suggest picturing a 10um-thick cell reflected by the 2D profile we simulate
+// suggest picturing a 10um-thick cell reflected by the 2D profile we simulate: 100x100x10 um = 1e5, for around 6e10 molecules / cell
 
 // Intracellular diffusion gradients of O2 and ATP https://journals.physiology.org/doi/epdf/10.1152/ajpcell.1986.250.5.C663
 
@@ -19,7 +21,7 @@
 
 #define RND drand48()
 
-int main(void)
+int main(int argc, char *argv[])
 {
   double *grid, *newgrid;
   int x[100], y[100];
@@ -36,130 +38,155 @@ int main(void)
   double totalATP, changeATP, minATP, maxATP, lastATP;
   double kappa, delta;
   double vol;
+  char masterfstr[100];
   
+  // different experiments: (0) uniform mitos, uniform consumption; (1) clustered mitos, uniform consumption;
+  //                        (2) uniform mitos, clustered consumption; (3) clustered mitos, clustered consumption
+  if(argc != 2) {
+    printf("Please specify an experiment! 0-3\n");
+    return 0;
+  }
+  expt = atoi(argv[1]);
+	   
   // allocate memory to store 100x100 grid of ATP concentration values
   grid = (double*)malloc(sizeof(double)*100*100);
   newgrid = (double*)malloc(sizeof(double)*100*100);
 
-  fp = fopen("stats.csv", "w");
+  // open file for output. we'll store statistics of each situation by timestep; we'll also (later) store snapshots of individual cases
+  sprintf(masterfstr, "stats-%i.csv", expt);
+  fp = fopen(masterfstr, "w");
   fprintf(fp, "expt,kappa,delta,t,total.ATP,ex.molar,min.ATP,max.ATP,change.ATP,consumption,terminated\n");
   fclose(fp);
 
   // total cell volume in dm-3: (in metres) * (dm3 in 1 m3)
   vol = (100*1e-6 * 100*1e-6 * 10*1e-6) * (10*10*10);
 	
-  // two different experiments: random mito spread or clustering near centre
-  for(expt = 0; expt <= 2; expt++)
+  // kappa is the rate constant of ATP consumption
+  for(kappa = 0.01; kappa <= 10; kappa *= 2)
     {
-      for(kappa = 0.01; kappa <= 10; kappa *= 2)
+      // delta is the production rate per mitochondrion
+      for(delta = 0.01; delta <= 10; delta *= 2)
 	{
-	  for(delta = 0.01; delta <= 10; delta *= 2)
+	  // output tracking info and initialise random seed for reproducibility
+	  printf("Running expt %i, kappa %.2e, delta %.2e\n", expt, kappa, delta);
+	  srand48(1);
+	      
+	  // initialises positions of mitochondria (x, y are coordinates)
+	  for(i = 0; i < 100; i++)
 	    {
-	      printf("Running expt %i, kappa %.2e, delta %.2e\n", expt, kappa, delta);
-	      srand48(1);
-	      // initialises positions of mitochondria (x, y are coordinates)
-	      for(i = 0; i < 100; i++)
-		{
-		  if(expt != 1) { x[i] = RND*100; y[i] = RND*100; }
-		  else { x[i] = RND*20+40; y[i] = RND*20+40; }
-		  for(j = 0; j < 100; j++)
-		    grid[i*100+j] = 0;
-		}
-
-	      // discrete time PDE solver (very crude)
-	      lastt = -1; changeATP = 1; totalATP = lastATP = 0;
-	      for(t = 0; t < 1000.1 && changeATP > dt*1e-4*totalATP; t+=dt)
-		{
-		  totalloss = totalgain = 0;
-		  // actual PDE "solver": loop through each element of our discretised domain
-		  for(i = 0; i < 100; i++)
-		    {
-		      for(j = 0; j < 100; j++)
-			{
-			  // find neighbours of this cell element -- using periodic boundary conditions
-			  l = (i == 0 ? grid[1*100+j] : grid[(i-1)*100+j]);
-			  r = (i == 99 ? grid[98*100+j] : grid[(i+1)*100+j]);
-			  u = (j == 0 ? grid[i*100+1] : grid[i*100+j-1]);
-			  d = (j == 99 ? grid[i*100+98] : grid[i*100+j+1]);
-			  h = grid[i*100+j];
-			  // finite difference solver with some parameters to capture diffusion
-			  newgrid[i*100+j] = grid[i*100+j] + 2.5e2*(dt*(l + r - 2*h) + dt*(u + d - 2*h));
-			  // loss of ATP. in proportion to current concentration; sites of loss depend on model
-			  if(expt == 0 || expt == 1)
-  			    loss = dt* newgrid[i*100+j]*kappa;
-			  else
-			    loss = ((i-50)*(i-50) + (j-50)*(j-50) < 25*25 ? dt* newgrid[i*100+j]*kappa*100 : 0);
-			  // impose nonnegativity (crudely and shouldn't happen
-			  if(newgrid[i*100+j]-loss < 0) newgrid[i*100+j] = 0;
-			  else { newgrid[i*100+j] -= loss; totalloss += loss; }
-			}
-		    }
-		  // the above took care of loss and diffusion, now take care of production terms
-		  // loop through the 100 mitos and add point ATP mass at each position
-		  for(i = 0; i < 100; i++)
-		    {
-		      newgrid[x[i]*100+y[i]] += delta*1e7*dt; // consumption is 10^9 per cell per sec
-		      totalgain += delta*1e7*dt;
-		    }
-		  // finally, update new state of cell from buffer
-		  totalATP = 0; minATP = 1e100; maxATP = 0; 
-		  for(i = 0; i < 100; i++)
-		    {
-		      for(j = 0; j < 100; j++)
-			{
-			  grid[i*100+j] = newgrid[i*100+j];
-			  if(grid[i*100+j] < minATP) minATP = grid[i*100+j];
-			  if(grid[i*100+j] > maxATP) maxATP = grid[i*100+j];
-			  totalATP += grid[i*100+j];
-			}
-		    }
-		  changeATP = totalATP - lastATP;
-		  lastATP = totalATP;
-	  
-		  if((int)t != lastt)
-		    {
-		      // printf("Total loss in last second %.2e\n", totalloss/dt);
-		      //printf("vol is %.2e dm3\n", vol);
-		      //printf("Total ATP = %.2e molecules (%.2e mol) (conc %.2eM)\n", totalATP, totalATP/6e23, totalATP/6e23 / vol);
-
-		      fp = fopen("stats.csv", "a");
-		      fprintf(fp, "%i,%.2e,%.2e,%i,%.2e,%.2e,%.2e,%.2e,%.2e,%.2e,0\n", expt, kappa, delta, (int)t, totalATP, totalATP/6e23/vol, minATP, maxATP, changeATP, totalloss/dt);
-		      fclose(fp);
-
-		      if((int)t < 5 || (int)t % 100 == 0)
-			{
-			  sprintf(fstr, "out-%i-%.2f-%.2f-%i.txt", expt, kappa, delta, (int)t);
-			  fp = fopen(fstr, "w");
-			  for(i = 0; i < 100; i++)
-			    {
-			      for(j = 0; j < 100; j++)
-				{
-				  fprintf(fp, "%i %i %.5f\n", i, j, grid[i*100+j]);
-				  total += grid[i*100+j];
-				}
-			      fprintf(fp, "\n");
-			    }
-			  fclose(fp);
-			}
-		      lastt = ((int)t);
-		    }
-
-		}
-	      printf("stopped at %e\n", t);
-	      fp = fopen("stats.csv", "a");
-	      fprintf(fp, "%i,%.2e,%.2e,%i,%.2e,%.2e,%.2e,%.2e,%.2e,%.2e,1\n", expt, kappa, delta, (int)t, totalATP, totalATP/6e23/vol, minATP, maxATP, changeATP, totalloss/dt);
-	      fclose(fp);
-
-	      // final output details of mitos
-	      sprintf(fstr, "mitos-%i.txt", expt);
-	      fp = fopen(fstr, "w");
-	      for(i = 0; i < 100; i++)
-		fprintf(fp, "%i %i\n", x[i], y[i]);
-	      fclose(fp);
+	      // if expt == 1, cluster near centre; otherwise spead evenly
+	      if(expt == 0 || expt == 1) { x[i] = RND*100; y[i] = RND*100; }
+	      if(expt == 2 || expt == 3) { x[i] = RND*20+40; y[i] = RND*20+40; }
+	      for(j = 0; j < 100; j++)
+		grid[i*100+j] = 0;
 	    }
+
+	  // discrete time PDE solver (very crude)
+	  // initialise change trackers
+	  lastt = -1; changeATP = 1; totalATP = lastATP = 0;
+
+	  // loop until a time threshold or until equilibration criterion is met
+	  for(t = 0; t < 1000.1 && changeATP > dt*1e-4*totalATP; t+=dt)
+	    {
+	      totalloss = totalgain = 0;
+	      // actual PDE "solver": loop through each element of our discretised domain
+	      for(i = 0; i < 100; i++)
+		{
+		  for(j = 0; j < 100; j++)
+		    {
+		      // find neighbours of this cell element -- using periodic boundary conditions
+		      l = (i == 0 ? grid[1*100+j] : grid[(i-1)*100+j]);
+		      r = (i == 99 ? grid[98*100+j] : grid[(i+1)*100+j]);
+		      u = (j == 0 ? grid[i*100+1] : grid[i*100+j-1]);
+		      d = (j == 99 ? grid[i*100+98] : grid[i*100+j+1]);
+		      h = grid[i*100+j];
+			  
+		      // finite difference solver with some parameters to capture diffusion
+		      newgrid[i*100+j] = grid[i*100+j] + 2.5e2*(dt*(l + r - 2*h) + dt*(u + d - 2*h));
+			  
+		      // loss of ATP. in proportion to current concentration; sites of loss depend on model
+		      if(expt == 0 || expt == 1)
+			loss = dt* newgrid[i*100+j]*kappa;
+		      if(expt == 2 || expt == 3)
+			loss = ((i-50)*(i-50) + (j-50)*(j-50) < 25*25 ? dt* newgrid[i*100+j]*kappa*100 : 0);
+			  
+		      // impose nonnegativity (crudely, and shouldn't happen)
+		      if(newgrid[i*100+j]-loss < 0) newgrid[i*100+j] = 0;
+		      else { newgrid[i*100+j] -= loss; totalloss += loss; }
+		    }
+		}
+	      // the above took care of loss and diffusion, now take care of production terms
+	      // loop through the 100 mitos and add point ATP mass at each position
+	      for(i = 0; i < 100; i++)
+		{
+		  newgrid[x[i]*100+y[i]] += delta*1e7*dt; // for reference, consumption is 10^9 per cell per sec and we have 10^2 mitos
+		  totalgain += delta*1e7*dt;
+		}
+	      // finally, update new state of cell from buffer
+	      totalATP = 0; minATP = 1e100; maxATP = 0; 
+	      for(i = 0; i < 100; i++)
+		{
+		  for(j = 0; j < 100; j++)
+		    {
+		      grid[i*100+j] = newgrid[i*100+j];
+		      if(grid[i*100+j] < minATP) minATP = grid[i*100+j];
+		      if(grid[i*100+j] > maxATP) maxATP = grid[i*100+j];
+		      totalATP += grid[i*100+j];
+		    }
+		}
+	      // record change in ATP for equilibration tracker
+	      changeATP = totalATP - lastATP;
+	      lastATP = totalATP;
+
+	      // if we're in a new timestep
+	      if((int)t != lastt)
+		{
+		  // output information, optionally
+		  // printf("Total loss in last second %.2e\n", totalloss/dt);
+		  //printf("vol is %.2e dm3\n", vol);
+		  //printf("Total ATP = %.2e molecules (%.2e mol) (conc %.2eM)\n", totalATP, totalATP/6e23, totalATP/6e23 / vol);
+
+		  // output stats snapshot
+		  fp = fopen(masterfstr, "a");
+		  fprintf(fp, "%i,%.2e,%.2e,%i,%.2e,%.2e,%.2e,%.2e,%.2e,%.2e,0\n", expt, kappa, delta, (int)t, totalATP, totalATP/6e23/vol, minATP, maxATP, changeATP, totalloss/dt);
+		  fclose(fp);
+
+		  // take full snapshots of early behaviour and subsequent changes
+		  if((int)t < 5 || (int)t % 100 == 0)
+		    {
+		      sprintf(fstr, "out-%i-%.2f-%.2f-%i.txt", expt, kappa, delta, (int)t);
+		      fp = fopen(fstr, "w");
+		      for(i = 0; i < 100; i++)
+			{
+			  for(j = 0; j < 100; j++)
+			    {
+			      fprintf(fp, "%i %i %.5f\n", i, j, grid[i*100+j]);
+			      total += grid[i*100+j];
+			    }
+			  fprintf(fp, "\n");
+			}
+		      fclose(fp);
+		    }
+		  lastt = ((int)t);
+		}
+
+	    }
+
+	  // this run terminated, either through equilibration or time runout
+	  printf("stopped at %e\n", t);
+	  fp = fopen(masterfstr, "a");
+	  fprintf(fp, "%i,%.2e,%.2e,%i,%.2e,%.2e,%.2e,%.2e,%.2e,%.2e,1\n", expt, kappa, delta, (int)t, totalATP, totalATP/6e23/vol, minATP, maxATP, changeATP, totalloss/dt);
+	  fclose(fp);
+
+	  // final output details of mitos
+	  sprintf(fstr, "mitos-%i.txt", expt);
+	  fp = fopen(fstr, "w");
+	  for(i = 0; i < 100; i++)
+	    fprintf(fp, "%i %i\n", x[i], y[i]);
+	  fclose(fp);
 	}
-      //      printf("%.5f %.5f\n", totalloss/dt, totalgain/dt);
     }
+
   return 0;
 }
 
--- plots.R
+++ plots.R
@@ -3,12 +3,17 @@ library(ggpubr)
 
 df = read.csv("stats.csv")
 
-### when is a reasonable fold range supported at equilibrium?
+###### RQ1. When is a reasonable fold range in ATP supported at equilibrium?
+# we are interested in whether bio-reasonable parameters can support high fold-changes in ATP conc
 
+# compute fold change across cell, and report for those instances that equilibrated
 df$fold.range = df$max.ATP/df$min.ATP
-df[df$terminated==1,]
+df$equilibrated = ifelse(df$terminated == 1 & df$t < 1000, 1, 0)
 
-df[df$terminated==1 & df$expt == 2,]
+# did anything fail to equilibrate?
+length(which(df$terminated == 1 & df$equilibrated != 1))
+
+# bio-reasonable values for consumption are around 10^9 ATP/cell/s; total ATP around 6e10 ATP/cell
 df[df$terminated==1 & 
      df$consumption > 5e8 & df$consumption < 5e9 & 
      df$total.ATP > 5e10 & df$fold.range > 1.,]
@@ -18,12 +23,15 @@ df[df$terminated==1 &
 df[df$terminated==1 & 
      df$total.ATP > 1e11 & df$fold.range > 1.,]
 
+# these plots aren't very helpful
+if(FALSE) {
+# offset for plotting
 dy = 0.25
 # what combinations support fold range at reasonable ATP levels?
 p.1 = ggplot(df[df$terminated==1,], aes(x=total.ATP, y=fold.range)) + 
-  geom_point() + 
-  geom_text(aes(x=total.ATP,y=fold.range,label=kappa), nudge_y=dy) + 
-  geom_text(aes(x=total.ATP,y=fold.range,label=delta), nudge_y=-dy) +
+  geom_point(size=1) + 
+  geom_text(aes(x=total.ATP,y=fold.range,label=kappa), nudge_y=dy, size=2) + 
+  geom_text(aes(x=total.ATP,y=fold.range,label=delta), nudge_y=-dy, size=2) +
   scale_x_continuous(transform = "log10") + facet_wrap(~expt, scales = "free")
 
 p.2 = ggplot(df[df$terminated==1,], aes(x=consumption, y=fold.range)) + 
@@ -37,14 +45,21 @@ p.2 = ggplot(df[df$terminated==1,], aes(x=consumption, y=fold.range)) +
 # ATP 1e11 with a 10um-thick cell gives ~1mM
 # ATP 1e10 with a 1um-thick cell gives ~1mM
 ggarrange(p.1, p.2, nrow=2)
+}
 
-p.3 = ggplot(df[df$terminated==1,], aes(x=consumption, y=total.ATP, size=log10(fold.range), label=kappa)) + 
-  geom_point() + 
+p.3 = ggplot() +
+  geom_rect(data = data.frame(xmin=5e8, xmax=5e9, ymin=5e9, ymax=5e11), 
+            aes(xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax),
+            fill = "lightblue") +
+  geom_point(data = df[df$terminated==1,], 
+             aes(x=consumption, y=total.ATP, size=log10(fold.range), color=log10(fold.range), label=kappa)) +
   scale_x_continuous(transform = "log10") + 
   scale_y_continuous(transform = "log10") + facet_wrap(~expt, scales = "free")
 
 p.3
+# so model 0 shows only limited maximal values; model 1 more; model 2 very high
 
+# check for convergence
 ggplot(df, aes(x=t, y=total.ATP, color=paste(kappa,delta))) + geom_point() + scale_x_continuous(transform = "log10")
 ggplot(df, aes(x=t, y=consumption, color=paste(kappa,delta))) + geom_point() + scale_x_continuous(transform = "log10")
 ggplot(df, aes(x=t, y=fold.range, color=paste(kappa,delta))) + geom_point() + 
@@ -52,23 +67,34 @@ ggplot(df, aes(x=t, y=fold.range, color=paste(kappa,delta))) + geom_point() +
 
 #### instances
 
-snaps = c("out-0-0.01-0.01-1.txt", "out-0-0.01-0.01-4.txt", "out-0-0.01-0.01-100.txt", "out-0-0.01-0.01-400.txt")
-snaps.m = rep("mitos-0.txt", 4)
-
-snaps = c("out-1-0.01-0.01-1.txt", "out-1-0.01-0.01-4.txt", "out-1-0.01-0.01-100.txt", "out-1-0.01-0.01-400.txt")
-snaps.m = rep("mitos-1.txt", 4)
-
-snaps = c("out-2-0.01-0.01-1.txt", "out-2-0.01-0.01-4.txt", "out-2-0.01-0.01-100.txt", "out-2-0.01-0.01-400.txt")
-snaps = c("out-2-0.08-5.12-1.txt", "out-2-0.08-5.12-4.txt", "out-2-2.56-0.01-100.txt", "out-2-2.56-0.01-400.txt")
-snaps.m = rep("mitos-2.txt", 4)
-
 p.list = list()
-for(i in 1:length(snaps)) {
-  t.snap = read.table(snaps[i], sep=" ", header=FALSE)
-  t.snap.m = read.table(snaps.m[i], sep=" ", header=FALSE)
-  p.list[[i]] = ggplot() +
-    geom_tile(data=t.snap, aes(x=V1,y=V2,fill=V3)) +
-    scale_fill_continuous(limits=c(0,NA)) +
-    geom_point(data=t.snap.m, aes(x=V1,y=V2), color="white", size=0.5)
+
+for(expt in 1:3) {
+  if(expt == 1) {
+    snaps = c("out-0-0.01-0.01-1.txt", "out-0-0.01-0.01-4.txt", "out-0-0.01-0.01-100.txt", "out-0-0.01-0.01-400.txt")
+    snaps.m = rep("mitos-0.txt", 4)
+  } else if(expt == 2) {
+    snaps = c("out-1-0.01-0.01-1.txt", "out-1-0.01-0.01-4.txt", "out-1-0.01-0.01-100.txt", "out-1-0.01-0.01-400.txt")
+    snaps.m = rep("mitos-1.txt", 4)
+  } else if(expt == 3) {
+    snaps = c("out-2-0.01-0.01-1.txt", "out-2-0.01-0.01-4.txt", "out-2-0.01-0.01-100.txt", "out-2-0.01-0.01-400.txt")
+    #snaps = c("out-2-0.08-5.12-1.txt", "out-2-0.08-5.12-4.txt", "out-2-2.56-0.01-100.txt", "out-2-2.56-0.01-400.txt")
+    snaps.m = rep("mitos-2.txt", 4)
+  }
+  
+  p.list[[expt]] = list()
+  for(i in 1:length(snaps)) {
+    t.snap = read.table(snaps[i], sep=" ", header=FALSE)
+    t.snap.m = read.table(snaps.m[i], sep=" ", header=FALSE)
+    p.list[[expt]][[i]] = ggplot() +
+      geom_tile(data=t.snap, aes(x=V1,y=V2,fill=log10(V3))) +
+      scale_fill_continuous(limits=c(0,NA)) +
+      geom_point(data=t.snap.m, aes(x=V1,y=V2), color="white", size=0.5) +
+      labs(x="", y="", fill="log10 [ATP]")
+  }
 }
-ggarrange(plotlist=p.list)
+
+ggarrange(ggarrange(plotlist=p.list[[1]], nrow=1),
+          ggarrange(plotlist=p.list[[2]], nrow=1),
+          ggarrange(plotlist=p.list[[3]], nrow=1), nrow=3)
+
