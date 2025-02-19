79768cd8b97495e336a5b7f3c5707b53242ea37a
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2025-02-19 14:37:39 +0100

    Comments

--- mito-agents.c
+++ mito-agents.c
@@ -7,6 +7,7 @@
 // typical cellular ATP conc = 1-10mM (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8615055/)
 // 1mM ATP = 10^-3 mol dm-3 = 10^-3 mol (10^15 um^3)-1 = 1e-3 * 6e23 / 1e15 = 6e5 molecules / um^3
 // suggest picturing a 10um-thick cell reflected by the 2D profile we simulate: 100x100x10 um = 1e5, for around 6e10 molecules / cell
+// update: cell dimensions are now parameters, so we can picture e.g. 50x50x10um
 
 // Intracellular diffusion gradients of O2 and ATP https://journals.physiology.org/doi/epdf/10.1152/ajpcell.1986.250.5.C663
 
@@ -20,9 +21,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-int NMITO = 100;
-int DEPTH = 10;
-int SUBDIV;   // number of simulation cells in 1um3, controls grid granularity
+#define MAXNMITO 1000  // lazy, for memory allocation
+
+int NMITO = 100;     // number of mitos in cell
+int DEPTH = 10;      // depth of cell in um
+int SUBDIV = 2;      // number of simulation cells in 1um3, controls grid granularity
 
 #define RND drand48()
 
@@ -57,7 +60,7 @@ int myround(double x)
 int main(int argc, char *argv[])
 {
   double *grid, *newgrid;
-  double x[NMITO], y[NMITO];
+  double x[MAXNMITO], y[MAXNMITO];
   FILE *fp;
   char fstr[100];
   double loss, totalloss, totalgain;
@@ -81,7 +84,8 @@ int main(int argc, char *argv[])
   double D = 2.5e2;
   double speed;
   double gridx;
-  
+
+  // process command-line arguments
   // different experiments: (0) uniform mitos, uniform consumption; (1) uniform mitos, clustered consumption;
   //                        (2) clustered mitos, uniform consumption; (3) clustered mitos, clustered consumption
   //                        (4) mobile-1 mitos, uniform consumption; (5) mobile-1 mitos, clustered consumption;
@@ -99,21 +103,29 @@ int main(int argc, char *argv[])
     }
     mparam = atof(argv[6]);
   }
+  // physical properties of cell
   cellsize = atoi(argv[1]);
   DEPTH = atoi(argv[2]);
   NMITO = atoi(argv[3]);
+  if(NMITO >= MAXNMITO) {
+    printf("Too many mitochondria!\n");
+    return 0;
+  }
 
+  // choose a timestep appropriate for our grid element scale (for numerical stability)
   if(SUBDIV <= 1) dt = 0.001;
   else if(SUBDIV <= 2) dt = 0.00025;
   else if(SUBDIV <= 4) dt = 0.00005;
-  
+
+  // helper geometric quantities
   gridx = 1./SUBDIV;
   GRIDX = GRIDY = cellsize*SUBDIV;
   CENTRE = GRIDX/2;
-  // circle radius CENTRE/2 -> pi (grid/4)^2 vs grid^2 -> 1./(pi/16.) 
-       CENTRESCALE = 1./(3.1416/16.);
   
-  // allocate memory to store GRIDXx100 grid of ATP concentration values
+  // circle radius CENTRE/2 -> pi (grid/4)^2 vs grid^2 -> 1./(pi/16.) 
+  CENTRESCALE = 1./(3.1416/16.);
+  
+  // allocate memory to store GRIDX*GRIDY grid of ATP concentration values
   grid = (double*)malloc(sizeof(double)*GRIDX*GRIDY);
   newgrid = (double*)malloc(sizeof(double)*GRIDX*GRIDY);
 
@@ -124,8 +136,6 @@ int main(int argc, char *argv[])
   fclose(fp);
 
   // total cell volume in dm-3: (in metres) * (dm3 in 1 m3)
-  // NB -- if you change GRIDX and GRIDY, space units will no longer correspond to um unless this is also changed
-  // old vol = (GRIDX*1e-6 * GRIDY*1e-6 * DEPTH*1e-6) * (10*10*10);
   vol = (cellsize*1e-6 * cellsize*1e-6 * DEPTH*1e-6) * (10*10*10);
 	
   // kappa is the rate constant of ATP consumption
@@ -148,13 +158,13 @@ int main(int argc, char *argv[])
 	      if(expt == 6 || expt == 7) { x[m] = RND*GRIDX; y[m] = RND*GRIDY; }
 	    }
 
+	  // initialise cell with zero ATP
 	  for(i = 0; i < GRIDX; i++)
 	    {
 	      for(j = 0; j < GRIDY; j++)
 		grid[i*GRIDY+j] = 0;
 	    }
 
-	  // discrete time PDE solver (very crude)
 	  // initialise change trackers
 	  lastt = -1; changeATP = 1; totalATP = lastATP = 0;
 
@@ -190,9 +200,13 @@ int main(int argc, char *argv[])
 			  
 		      // loss of ATP. in proportion to current concentration; sites of loss depend on model
 		      if(expt == 0 || expt == 2 || expt == 4 || expt == 6)
-			loss = dt* newgrid[i*GRIDY+j]*kappa;
+			{
+			  // loss throughout cell
+			  loss = dt* newgrid[i*GRIDY+j]*kappa;
+			}
 		      if(expt == 1 || expt == 3 || expt == 5 || expt == 7)
 			{
+			  // loss only in central circular region
 			  if((i-CENTRE)*(i-CENTRE) + (j-CENTRE)*(j-CENTRE) < (CENTRE*CENTRE/4))
 			    loss = dt* newgrid[i*GRIDY+j]*kappa*CENTRESCALE;
 			  else
@@ -217,6 +231,7 @@ int main(int argc, char *argv[])
 		    h = grid[i*GRIDY+j];
 
 		    if(expt == 4 || expt == 5) {
+		      // random diffusion with width mparam * [ATP] um / second
 		      dx = gsl_ran_gaussian(mparam*h/(gridx*gridx));
 		      dy = gsl_ran_gaussian(mparam*h/(gridx*gridx));
 		    }
@@ -226,10 +241,12 @@ int main(int argc, char *argv[])
 		      norm = sqrt(gradx*gradx + grady*grady);
 		      if(norm == 0)
 			{
+			  // some small perturbation if concentration is completely flat (shouldn't happen)
 			  dx = gsl_ran_gaussian(1./gridx);
 			  dy = gsl_ran_gaussian(1./gridx);
 			}
 		      else {
+			// step size U(0,1) * mparam * [x component of gradient] um / second
 			double rnd = RND;
 			dx = -(mparam/gridx)*rnd*gradx/norm;
 			dy = (mparam/gridx)*rnd*grady/norm;
@@ -237,18 +254,14 @@ int main(int argc, char *argv[])
 			//  printf("%f %i: %f , l %f r %f d %f u %f, dx %f dy %f\n", t, m, h, l, r, d, u, dx, dy);
 		      }
 		    }
-		    // limit to 1 um/s
-		    /*		    speed = sqrt(dx*dx+dy*dy);
-	     	    if(speed > SUBDIV) {
-		      dx /= (speed/SUBDIV);
-		      dy /= (speed/SUBDIV);
-		      }*/
+		    // we want to limit to 1 um/s maximum, but do this through empirical choice of the mparam values, not explicitly here
 		    x[m] += dx*dt;
 		    y[m] += dy*dt;
 		    if(x[m] > 2*GRIDX || y[m] > 2*GRIDY || x[m] < -GRIDX || y[m] < -GRIDY) {
 		      printf("Way out of bounds!\n");
 		      return 0;
 		    }
+		    // reflecting boundaries
 		    if(x[m] > GRIDX-1) x[m] = GRIDX-1;
 		    if(x[m] < 0) x[m] = 0;
 		    if(y[m] > GRIDY-1) y[m] = GRIDY-1;
@@ -259,7 +272,9 @@ int main(int argc, char *argv[])
 	      // loop through the NMITO mitos and add point ATP mass at each position
 	      for(m = 0; m < NMITO; m++)
 		{
-		  newgrid[myround(x[m])*GRIDY+myround(y[m])] += delta*1e7*dt; // for reference, consumption is 10^9 per cell per sec and we have 10^2 mitos
+		  newgrid[myround(x[m])*GRIDY+myround(y[m])] += delta*1e7*dt;
+		  // for reference, consumption is 10^9 per cell per sec and we have (default) 10^2 mitos
+		  // we're scaling through orders of magnitude for delta though so will see if we're in a reasonable region by tracking the cellular total 
 		  totalgain += delta*1e7*dt;
 		}
 
@@ -272,6 +287,7 @@ int main(int argc, char *argv[])
 		      grid[i*GRIDY+j] = newgrid[i*GRIDY+j];
 		      if(grid[i*GRIDY+j] < minATP) minATP = grid[i*GRIDY+j];
 		      if(grid[i*GRIDY+j] > maxATP) maxATP = grid[i*GRIDY+j];
+		      // contribute to cellular ATP statistics (total/mean and variance)
 		      totalATP += grid[i*GRIDY+j];
 		      totalATP2 += grid[i*GRIDY+j]*grid[i*GRIDY+j];
 		    }
@@ -279,6 +295,8 @@ int main(int argc, char *argv[])
 	      // record change in ATP for equilibration tracker
 	      changeATP = totalATP - lastATP;
 	      lastATP = totalATP;
+
+	      // statistics of ATP: mean and variance
 	      meanATP = totalATP/(GRIDX*GRIDY); meanATP2 = totalATP2/(GRIDX*GRIDY);
 	      varATP = meanATP2 - meanATP*meanATP; 
 
@@ -291,6 +309,8 @@ int main(int argc, char *argv[])
 		  //printf("Total ATP = %.2e molecules (%.2e mol) (conc %.2eM)\n", totalATP, totalATP/6e23, totalATP/6e23 / vol);
 
 		  // output stats snapshot
+		  // ATP concentrations here are output as molecule number per simulation cell
+		  // needs accounting for if we compare different simulation cell sizes or depths
 		  fp = fopen(masterfstr, "a");
 		  fprintf(fp, "%i,%e,%.2e,%.2e,%i,%.2e,%.2e,%.2e,%.2e,%.2e,%.2e,%.2e,%.2e,%.2e,0\n", expt, mparam, kappa, delta, (int)t, totalATP, totalATP/6e23/vol, minATP, maxATP, changeATP, meanATP, varATP, vol, totalloss/dt);
 		  fclose(fp);
@@ -335,6 +355,7 @@ int main(int argc, char *argv[])
 	    {
 	      for(j = 0; j < GRIDY; j++)
 		{
+		  // this outputs simply ATP molecules number per simulation cell
 		  fprintf(fp, "%i %.4f %.4f %.5f\n", (int) t, (double)i*gridx, (double)j*gridx, grid[i*GRIDY+j]);
 		  total += grid[i*GRIDY+j];
 		}
